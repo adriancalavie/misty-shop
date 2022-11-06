e46491d159bd4c89cdbdca67f400f10ac659b2a5
Adrian Calavie <adrian.calavie06@gmail.com>
2022-11-06 23:50:28 +0200

    add posts component

---
+++ frontend/src/components/UserList.vue
@@ -0,0 +1,13 @@
+<template></template>
+
+<script setup>
+import { ref, computed } from "vue";
+
+const data = ref(null);
+const loading = ref(true);
+const error = ref(null);
+
+function fetchData() {
+  // TODO Implement this
+}
+</script>
