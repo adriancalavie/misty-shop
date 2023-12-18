b80fc9d5e1c59c001e21a3127b43c6f2bf03cecf
Adrian Călăvie <adrian.calavie06@gmail.com>
2023-12-18 02:05:15 +0200

    Update backend to use supabase

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,96 +1,36 @@
-use axum::{
-    http::{HeaderValue, Method, StatusCode},
-    response::IntoResponse,
-    routing::{get, post},
-    Json, Router,
-};
-// use chrono::{DateTime, Duration, Utc};
-// use rand::{thread_rng, Rng};
-use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
-use tower_http::cors::CorsLayer;
-use uuid::Uuid;
+use dotenv::dotenv;
+use postgrest::Postgrest;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Item {
+    id: i32,
+    created_at: String,
+    name: String,
+}
 
 #[tokio::main]
-async fn main() {
-    // initialize tracing
-    tracing_subscriber::fmt::init();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok(); // Load the environment variables from the .env file
 
-    // TODO init Surreal db
+    let supabase_anon_key =
+        std::env::var("SUPABASE_ANON_KEY").expect("SUPABASE_ANON_KEY not set in .env");
 
-    // build our application with a route
-    let app = Router::new()
-        // `GET /` goes to `root`
-        .route("/", get(root))
-        // `POST /users` goes to `create_user`
-        .route("/user", post(create_user))
-        .route(
-            "/users",
-            get(get_users).layer(
-                CorsLayer::new()
-                    // "*" allows all the path to have CORS access
-                    .allow_origin("*".parse::<HeaderValue>().unwrap())
-                    .allow_methods([Method::GET]),
-            ),
-        );
+    let client = Postgrest::new("https://hvutwgemvirmzqqycerg.supabase.co/rest/v1")
+        .insert_header("apikey", supabase_anon_key);
 
-    // run our app with hyper
-    // `axum::Server` is a re-export of `hyper::Server`
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-    tracing::debug!("listening on {}", addr);
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+    let resp = client.from("items").select("*").execute().await?;
+    // println!("Response: {:?}", resp);
 
-// basic handler that responds with a static string
-async fn root() -> &'static str {
-    "Hello, World!"
-}
+    if !resp.status().is_success() {
+        eprintln!("Error: {}", resp.text().await?);
+    } else {
+        let objects: Vec<Item> = resp.json().await?;
 
-async fn new_user(name: String) -> User {
-    User {
-        id: Uuid::new_v4().to_string(),
-        username: name,
+        for obj in objects {
+            println!("{:?}", obj);
+        }
     }
-}
 
-async fn create_user(
-    // this argument tells axum to parse the request body
-    // as JSON into a `CreateUser` type
-    Json(payload): Json<CreateUser>,
-) -> impl IntoResponse {
-    // insert your application logic here
-    let user: User = new_user(payload.username).await;
-
-    // TODO Save user in db
-
-    // this will be converted into a JSON response
-    // with a status code of `201 Created`
-    (StatusCode::CREATED, Json(user))
-}
-
-async fn get_users() -> impl IntoResponse {
-    let users = vec![
-        new_user("Adrian".to_owned()).await,
-        new_user("Diana".to_owned()).await,
-        new_user("Dinu".to_owned()).await,
-    ];
-
-    (StatusCode::OK, Json(users))
-}
-
-// the input to our `create_user` handler
-#[derive(Deserialize)]
-struct CreateUser {
-    username: String,
-}
-
-// the output to our `create_user` handler
-#[derive(Serialize)]
-struct User {
-    // TODO Check if it's safe to store uuid as string
-    id: String,
-    username: String,
+    Ok(())
 }
