6e1df238326930dc80ca69cc59b358d0a7e472cc
Adrian Calavie <adrian.calavie06@gmail.com>
2023-12-23 14:03:29 +0200

    Add more screen sizes

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -8,7 +8,9 @@
 
 <h1>HOME</h1>
 <Button on:click={() => (dataImported = !dataImported)}>Toggle</Button>
-<div class="mx-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+<div
+	class="3xl:grid-cols-6 4xl:grid-cols-7 mx-10 grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-5"
+>
 	{#each ids as id}
 		<Item {id} />
 	{/each}
